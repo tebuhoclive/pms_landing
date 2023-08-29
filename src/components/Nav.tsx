fb00d82e35c66c2ea3d433b459f8c6f7986b7e7d
unknown <tebuhoclive14@gmail.com>
2023-08-29 20:04:56 +0200

    Nav response

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,9 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav: React.FC = () => {
   return (
-    <nav
-      className="uk-navbar-container uk-navbar-transparent uk-padding  uk-flex uk-flex-center"
-    >
+    <nav className="uk-navbar-container uk-navbar-transparent uk-padding uk-flex uk-flex-center">
       <div
         className="uk-navbar"
         style={{
@@ -13,22 +12,23 @@ const Nav: React.FC = () => {
           backgroundColor: "white",
           borderRadius: "30px",
           display: "flex",
-          justifyContent: "space-between", // Distribute items evenly along the container
-          alignItems: "center", // Center items vertically
+          justifyContent: "space-between",
+          alignItems: "center",
         }}
       >
+        {/* Main Menu */}
         <ul
-          className="uk-navbar-nav uk-flex uk-flex-row uk-text-center"
+          className="uk-navbar-nav uk-flex uk-flex-row uk-text-center uk-margin-remove-bottom" // Remove bottom margin for mobile
           style={{
             padding: "0 20px",
-            paddingRight:'1px',
+            paddingRight: "1px",
             margin: 0,
             listStyle: "none",
             maxWidth: "100%",
             width: "100%",
             display: "flex",
-            justifyContent: "flex-start", // Move items to the start (left) of the container
-            alignItems: "center", // Center items vertically
+            justifyContent: "flex-start",
+            alignItems: "center",
           }}
         >
           <li className="uk-margin-right">
@@ -46,14 +46,39 @@ const Nav: React.FC = () => {
             <Link to="/testimonies">Contact Us</Link>
           </li>
         </ul>
-        <ul className="uk-navbar-nav ">
-          <li className="uk-margin-right">
-            <Link to="/login">Login</Link>
-          </li>
-          <li className="uk-margin-right">
-            <Link to="/signup">Signup</Link>
-          </li>
-        </ul>
+
+        {/* Mobile Menu Toggle */}
+        <label htmlFor="mobile-toggle" className="uk-margin-remove-bottom">
+          <span
+            className="uk-icon uk-navbar-toggle-icon"
+            uk-navbar-toggle-icon=""
+          ></span>
+        </label>
+        <input id="mobile-toggle" type="checkbox" className="uk-hidden" />
+
+        {/* Mobile Menu */}
+        <div
+          className="uk-navbar-dropdown"
+          style={{ backgroundColor: "white", borderRadius: "30px" }}
+        >
+          <ul className="uk-nav uk-navbar-dropdown-nav">
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/testimonies">Functions</Link>
+            </li>
+            <li>
+              <Link to="/testimonies">Contact Us</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
